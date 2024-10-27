0f755a95355deefa956149981805f725fca6e922
hallhu1 <hallhu@umich.edu>
2024-10-26 20:58:54 -0400

    Changed CSX and LCD_RST pins to be default high

--- fretboard-dev/Core/Src/main.c
+++ fretboard-dev/Core/Src/main.c
@@ -320,10 +320,10 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOA, USER_LED_Pin|LCD_RS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, LCD_RST_Pin|CSX_A_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, LCD_RST_Pin|CSX_A_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, CSX_B_Pin|CSX_C_Pin|CSX_D_Pin|CSX_E_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, CSX_B_Pin|CSX_C_Pin|CSX_D_Pin|CSX_E_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : B1_BLUE_USER_BUTTON_Pin */
   GPIO_InitStruct.Pin = B1_BLUE_USER_BUTTON_Pin;
