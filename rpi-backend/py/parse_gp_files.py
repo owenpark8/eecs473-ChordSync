dbaad2824e99464c05b9f4fa0e6b9d20158e39be
Hudson Hall <hallhu@umich.edu>
2024-12-02 23:00:56 -0500

    Python script for parsing gpx files

---
+++ rpi-backend/py/parse_gp_files.py
@@ -0,0 +1,48 @@
+import sys
+import guitarpro
+
+DEBUG = False
+
+def debug_print(message):
+    if DEBUG:
+        print(message)
+# array to convert string number to midi note value
+# High E (64) to low E (40)
+string_dict = [64, 59, 55, 50, 45, 40]
+
+# parses gpx file and then outputs vector of ints [[note value, start_timestamp_ms, length_ms, int fret, int string]]
+# fret == 0 means open string
+def parse_gp_file(gp_file_path):
+    song = guitarpro.parse(gp_file_path)
+
+    debug_print(f"Available tracks:")
+    for track in song.tracks:
+        debug_print(f"Track {track.number}: {track.name}")
+    debug_print("----------------------------------------------") # empty line
+    song_info_out = []
+    track = song.tracks[0] # the first track is usually the correct one / the main part
+    timestamp = 0.0
+    for measure in track.measures:
+        for voice in measure.voices:
+            for beat in voice.beats:
+                # Iterate over notes in the beat
+                for note in beat.notes:
+                    midi_note_val = string_dict[note.string-1] + note.value # string is 1-indexed
+                    fret_number = note.value
+                    string_number = note.string - 1 # string is 1-indexed
+                    length = beat.duration.time
+                    _, _, note_type = str(note.type).partition('.')
+
+                    debug_print(f"MidiNote val: {midi_note_val}, Start timestamp: {timestamp:.2f} ms, Length: {length}, Fret: {fret_number}, String: {string_number}, NoteType: {note_type}")
+                    song_info_out.append([midi_note_val, timestamp, length, fret_number, string_number])
+
+                # Increment timestamp
+                timestamp += beat.duration.time
+    return song_info_out
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python read_notes.py <path_to_gp_file>")
+    else:
+        gp_file_path = sys.argv[1]
+        parse_gp_file(gp_file_path)
