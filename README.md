0da8034a5b1cdf8c5a0414964c45dc7c3a4c263e
owenpark <owenmmp@gmail.com>
2024-12-04 15:01:08 -0500

    rpi setup scripts

---
+++ README.md
@@ -0,0 +1,16 @@
+# ChordSync
+
+## Setting up the Raspberry Pi
+
+```sh
+sudo apt install git cmake
+```
+
+```sh
+git clone https://github.com/owenpark8/eecs473-VSGuitar.git
+cd eecs473-VSGuitar
+```
+
+```sh
+./scripts/setup.sh
+```
---
+++ rpi-backend/scripts/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd rpi-backend
+
+# CMake
+
+CMAKE_DIR="cmake-build"
+if [ -d "$CMAKE_DIR" ]; then
+  echo "CMake directory '$CMAKE_DIR' exists. Replacing it with an empty directory."
+  rm -rf "$CMAKE_DIR"
+else
+  echo "CMake directory '$CMAKE_DIR' does not exist."
+fi
+
+echo "Creatking CMake directory '$CMAKE_DIR'."
+mkdir "$CMAKE_DIR"
+
+cd "$CMAKE_DIR"
+cmake .. -DCMAKE_BUILD_TYPE=Release
+
+# Python
+
