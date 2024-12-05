631cbca344e1e8cb6d8af129da40d6dacadf9deb
owenpark <owenmmp@gmail.com>
2024-12-05 01:04:20 -0500

    lower uart baudrate for stability

--- lib/timing.hpp
+++ lib/timing.hpp
@@ -7,6 +7,6 @@ namespace timing {
 
     constexpr std::chrono::seconds SONG_START_DELAY = 3s;
     constexpr std::chrono::milliseconds NOTE_WARNING_DELAY = 500ms;
-    constexpr unsigned int UART_BAUDRATE = 115200;
+    constexpr unsigned int UART_BAUDRATE = 9600;
 
 } // namespace timing
